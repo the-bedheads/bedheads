0b5c5708a5e7fa04e4ee2042f063eba1a7aec619
mconner89 <john.conner@eagles.usm.edu>
2020-10-27 23:01:43 -0500

    (feat) calendar

--- src/components/dashboard/availability/Calendar.tsx
+++ src/components/dashboard/availability/Calendar.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 
-const Calendar = () => (
+const localizer = momentLocalizer(moment);
+console.log(localizer);
+
+const UserCalendar = () => (
   <>
     Availability Calendar
   </>
 );
 
-export default Calendar;
+export default UserCalendar;
