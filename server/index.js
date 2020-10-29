9fa2ea1440ba44abc30e830c8584b0e413edcd7f
Greg <gregthompson27@gmail.com>
2020-10-29 18:02:48 -0500

    (add) server listeners for user, listing, and availability routes

--- server/index.js
+++ server/index.js
@@ -3,9 +3,16 @@ const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 require('./db/index.js');
+const { listingRouter } = require('./db/routes/listingRoutes');
+const { userRouter } = require('./db/routes/userRoutes');
+const { availabilityRouter } = require('./db/routes/availabilityRoutes');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use('/listing', listingRouter);
+app.use('/user', userRouter);
+app.use('/availability', availabilityRouter);
 
 app.use(express.static(path.join(__dirname, '../build')));
 app.use(bodyParser.json());
