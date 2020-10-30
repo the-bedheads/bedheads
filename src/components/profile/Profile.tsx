04efba08d13c82185bb38fe9dd190630195f3563
mconner89 <john.conner@eagles.usm.edu>
2020-10-30 16:40:36 -0500

    (feat) profile

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import ProfileInfo from './ProfileInfo';
+import SidebarInfo from './SidebarInfo';
 
-const Profile = () => (
+const Profile: React.FC = (): JSX.Element => (
   <>
-    General Profile Page
+    <Grid container spacing={2}>
+      <Grid item xs={3}>
+        <SidebarInfo />
+      </Grid>
+      <Grid item xs={9}>
+        <ProfileInfo />
+      </Grid>
+    </Grid>
   </>
 );
 
