7b12dc43625a332932d28c8ad365828160100178
Jess Richmond <yesjessrichmond@gmail.com>
2020-10-31 10:29:24 -0500

    (add) mapbox integration

--- src/components/global/Map.tsx
+++ src/components/global/Map.tsx
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Map = () => (
-  <>
-    Map
-  </>
-);
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import axios from 'axios';
+
+const Map = (props: any) => {
+  const [viewport, setViewport] = useState({
+    latitude: 29.959021,
+    longitude: -90.065285,
+    zoom: 12,
+    bearing: 0,
+    pitch: 0,
+  });
+  const { locationQuery } = props;
+
+  const geocodeQuery = (query: string) => {
+    axios.get(`/api/geocode/${query}`)
+      .then((response) => {
+        console.log('RESPONSE', response.data.features[0].center);
+        setViewport({
+          latitude: response.data.features[0].center[1],
+          longitude: response.data.features[0].center[0],
+          zoom: 12,
+          bearing: 0,
+          pitch: 0,
+        });
+      });
+  };
+
+  useEffect(() => {
+    if (locationQuery.length) {
+      geocodeQuery(locationQuery);
+    }
+  }, [locationQuery]);
+
+  return (
+    <div className="mapbox-react">
+      <ReactMapGL
+        latitude={viewport.latitude}
+        longitude={viewport.longitude}
+        zoom={viewport.zoom}
+        width="500px"
+        height="700px"
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        onViewportChange={(nextViewport: React.SetStateAction<
+        { latitude: number;
+          longitude: number;
+          zoom: number;
+          bearing: number;
+          pitch: number; }>) => setViewport(nextViewport)}
+      >
+        <div style={{ position: 'absolute', right: 10, top: 10 }}>
+          <NavigationControl
+            onViewportChange={(nextViewport: React.SetStateAction<
+            { latitude: number;
+              longitude: number;
+              zoom: number;
+              bearing: number;
+              pitch: number; }>) => setViewport(nextViewport)}
+          />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
+};
 
 export default Map;
