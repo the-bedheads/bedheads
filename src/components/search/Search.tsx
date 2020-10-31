8128e4b18d79d2bcb3ac5431da7888cee4a37f2b
Jess Richmond <yesjessrichmond@gmail.com>
2020-10-31 10:35:17 -0500

    (add) connection between search and map via hooks

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
+import Map from '../global/Map';
+import SearchBar from './SearchBar';
+import ResultsList from './ResultsList';
 
-const Search = () => (
-  <>
-    General Search Page
-  </>
-);
+type SearchProps = {
+  query: string
+};
+
+const Search: React.FC = () => {
+  const [locationQuery, setLocationQuery] = useState('');
+
+  return (
+    <>
+      <SearchBar onSubmit={(value: any) => setLocationQuery(value)} />
+      <Map locationQuery={locationQuery} />
+      <ResultsList />
+    </>
+  );
+};
 
 export default Search;
--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { SetStateAction, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Button, Input } from '@material-ui/core';
 
-const SearchBar = () => (
-  <>
-    Search Bar
-  </>
-);
+type Search = {
+  locationQuery: string;
+};
+
+const SearchBar = (props: any) => {
+  const { register, handleSubmit } = useForm<Search>();
+  const onSubmit = (data: Search) => {
+    props.onSubmit(data.locationQuery);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* <label htmlFor="my-input">location to search</label> */}
+        <Input
+          name="locationQuery"
+          inputRef={register}
+          aria-describedby="my-helper-text"
+        />
+        {/* <FormHelperText id="my-helper-text">We will never share your email.</FormHelperText> */}
+        <Button
+          type="submit"
+        >
+          SEARCH
+        </Button>
+      </form>
+    </>
+  );
+};
 
 export default SearchBar;
