b55a3705c7835c74528d9eb937b7bc21577a1081
Greg <gregthompson27@gmail.com>
2020-10-31 21:23:34 -0500

    (update) availability and listing db models
    Availabilities now belong to users in addition to listings.
    Listing model now has address broken up into city, state, zip.
    Sample data updated to work with updated models.

--- server/test/sampleData/availabilities.js
+++ server/test/sampleData/availabilities.js
@@ -4,125 +4,143 @@ module.exports = [
     startDate: new Date(2020, 10, 31),
     endDate: new Date(2020, 11, 2),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 4,
   },
   {
     listing_id: 5,
     startDate: new Date(2020, 10, 31),
     endDate: new Date(2020, 11, 2),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 5,
   },
   {
     listing_id: 1,
     startDate: new Date(2020, 11, 6),
     endDate: new Date(2020, 11, 9),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 1,
   },
   {
     listing_id: 2,
     startDate: new Date(2020, 11, 6),
     endDate: new Date(2020, 11, 9),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 2,
   },
   {
     listing_id: 3,
     startDate: new Date(2020, 11, 5),
     endDate: new Date(2020, 11, 9),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 3,
   },
   {
     listing_id: 4,
     startDate: new Date(2020, 11, 15),
     endDate: new Date(2020, 11, 21),
     accepted: true,
-    guest_id: 1
+    guest_id: 1,
+    host_id: 4,
   },
   {
     listing_id: 1,
     startDate: new Date(2020, 11, 15),
     endDate: new Date(2020, 11, 21),
     accepted: true,
-    guest_id: 4
+    guest_id: 4,
+    host_id: 1,
   },
   {
     listing_id: 5,
     startDate: new Date(2020, 11, 20),
     endDate: new Date(2020, 11, 22),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 5,
   },
   {
     listing_id: 2,
     startDate: new Date(2020, 11, 22),
     endDate: new Date(2020, 11, 24),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 2,
   },
   {
     listing_id: 3,
     startDate: new Date(2020, 11, 22),
     endDate: new Date(2020, 11, 28),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 3,
   },
   {
     listing_id: 1,
     startDate: new Date(2020, 11, 28),
     endDate: new Date(2020, 11, 29),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 1,
   },
   {
     listing_id: 2,
     startDate: new Date(2020, 11, 28),
     endDate: new Date(2020, 11, 29),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 2,
   },
   {
     listing_id: 5,
     startDate: new Date(2020, 11, 28),
     endDate: new Date(2020, 11, 29),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 5,
   },
   {
     listing_id: 4,
     startDate: new Date(2020, 11, 28),
     endDate: new Date(2020, 11, 29),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 4,
   },
   {
     listing_id: 1,
     startDate: new Date(2020, 11, 30),
     endDate: new Date(2020, 12, 3),
     accepted: false,
-    guest_id: null
+    guest_id: null,
+    host_id: 1,
   },
   {
     listing_id: 3,
     startDate: new Date(2020, 12, 7),
     endDate: new Date(2020, 12, 10),
     accepted: true,
-    guest_id: 5
+    guest_id: 5,
+    host_id: 3,
   },
   {
     listing_id: 5,
     startDate: new Date(2020, 12, 7),
     endDate: new Date(2020, 12, 10),
     accepted: true,
-    guest_id: 3
+    guest_id: 3,
+    host_id: 5,
   },
   {
     listing_id: 4,
     startDate: new Date(2020, 12, 7),
     endDate: new Date(2020, 12, 11),
     accepted: false,
-    guest_id: null
-  }
+    guest_id: null,
+    host_id: 4,
+  },
 ];
