515d6a7e21008672521a3c7a949242029702d1f9
Greg <gregthompson27@gmail.com>
2020-11-01 14:53:16 -0600

    (update) routes to send info to dashboard
    Also remove console logs from routes. Replace with 500 errors.

--- server/db/routes/dashboardRoutes.js
+++ server/db/routes/dashboardRoutes.js
@@ -17,17 +17,39 @@ const dashboardRouter = Router();
 
 dashboardRouter
   .get('/', (req, res) => {
-    console.log('GOT A PING TO THE DASHBOARD ROUTER!!!!!');
     const results = {};
     const { userId, listingId } = req.query;
-    // axios.get('/listing', )
-    const url = `http://localhost:3000/availability/countSwaps/${userId}`;
-    console.log(url);
-    axios.get(url)
-    // console.log(swapCount);
-      .then((swaps) => console.log('swaps:', swaps.data))
-      .catch((err) => console.log('ERROR', err, 'ERROR'));
-    res.send('hello!!!!!!');
+    const swapCountUrl = `http://localhost:3000/availability/countSwaps/${userId}`;
+    axios.get(swapCountUrl)
+      .then((swaps) => {
+        results.confirmedSwapCount = swaps.data.count;
+        const availabilityArrayUrl = `http://localhost:3000/availability/mineIdOnly/${userId}`;
+        axios.get(availabilityArrayUrl)
+          .then((arr) => {
+            const { data } = arr;
+            const countRequestUrl = 'http://localhost:3000/request/countByArray';
+            axios.get(countRequestUrl, {
+              params: {
+                arr: data,
+              },
+            })
+              .then((requests) => {
+                results.pendingRequests = requests.data;
+                const otherAvailabilitiesUrl = `http://localhost:3000/availability/others/currentUser/${userId}`;
+                axios.get(otherAvailabilitiesUrl)
+                  .then((avails) => {
+                    const { data } = avails;
+                    results.openAvailabilities = data;
+                    res.send(results);
+                  })
+                  .catch((err) => res.status(500).send(err));
+                // res.send(results);
+              })
+              .catch((err) => res.status(500).send(err));
+          })
+          .catch((err) => res.status(500).send(err));
+      })
+      .catch((err) => res.status(500).send(err));
   });
 
 module.exports = {
