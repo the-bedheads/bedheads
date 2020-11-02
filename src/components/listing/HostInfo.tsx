c8b299c7cf0760830280d727ed976655f93e275d
Jess Richmond <yesjessrichmond@gmail.com>
2020-11-01 21:35:12 -0600

    (add) components for listing view

--- src/components/listing/HostInfo.tsx
+++ src/components/listing/HostInfo.tsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
 
-const HostInfo = () => (
-  <>
-    Host Info Box
-  </>
-);
+const HostInfo = (props: any) => {
+  const { hostId } = props;
+  const [hostData, setHostData] = useState({
+    firstName: '',
+    lastName: '',
+    pronouns: '',
+    hostRating: '',
+  });
+
+  const getHost = () => {
+    axios.get(`/user/${hostId}`)
+      .then((hostInfo) => {
+        const {
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          first_name, last_name, pronouns, hostRating,
+        } = hostInfo.data;
+        setHostData({
+          firstName: first_name,
+          lastName: last_name,
+          pronouns,
+          hostRating,
+        });
+      })
+      .catch((err) => err);
+  };
+
+  useEffect(() => {
+    getHost();
+  }, []);
+
+  return (
+    <div className="host-info">
+      <h3>host info</h3>
+      <img src="https://i.ibb.co/ZMtTcsm/bettythedog.jpg" alt="dog portrait" width="150" />
+      <br />
+      {hostData.firstName}
+      {' '}
+      {hostData.lastName}
+      {' '}
+      (
+      {hostData.pronouns}
+      )
+      <br />
+      host rating:
+      {' '}
+      {hostData.hostRating}
+      <br />
+      <Button variant="outlined" color="secondary">
+        message host
+      </Button>
+      <br />
+      <br />
+      <Button variant="outlined" color="secondary">
+        request swap
+      </Button>
+    </div>
+  );
+};
 
 export default HostInfo;
--- src/components/listing/Listing.tsx
+++ src/components/listing/Listing.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-const Listing = () => (
-  <>
-    Singular Listing
-  </>
-);
+import ListingInfo from './ListingInfo';
+import HostInfo from './HostInfo';
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    flewGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const Listing = () => {
+  const classes = useStyles();
+  const { id } = useParams<{ id: any }>();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={8}>
+          <Paper className={classes.paper}>
+            <ListingInfo listingId={id} />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={2}>
+          <Paper className={classes.paper}>
+            <HostInfo hostId={id} />
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default Listing;
