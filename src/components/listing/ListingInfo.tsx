39e63b0a36407f5022fb618b62ec8b86b4a65c3a
Jess Richmond <yesjessrichmond@gmail.com>
2020-11-01 21:35:12 -0600

    (add) components for listing view

--- src/components/listing/ListingInfo.tsx
+++ src/components/listing/ListingInfo.tsx
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const ListingInfo = () => (
-  <>
-    Listing Info
-  </>
-);
+const ListingInfo = (props: any) => {
+  const { listingId } = props;
+  const [listingData, setListingData] = useState({
+    listingTitle: null,
+    listingDescription: null,
+    listingAddress: null,
+    ada: null,
+    internet: null,
+    pets: null,
+    privateBath: null,
+    roommates: null,
+    smoking: null,
+  });
+
+  const getListing = () => {
+    axios.get(`/listing/user/${listingId}`)
+      .then((listingInfo) => {
+        const {
+          listingTitle, listingDescription, listingAddress, ada,
+          internet, pets, privateBath, roommates, smoking,
+        } = listingInfo.data;
+        setListingData({
+          listingTitle,
+          listingDescription,
+          listingAddress,
+          ada,
+          internet,
+          pets,
+          privateBath,
+          roommates,
+          smoking,
+        });
+      })
+      .catch((err) => err);
+  };
+
+  useEffect(() => {
+    getListing();
+  }, []);
+
+  return (
+    <div className="listing-info">
+      <h3>LISTING INFO</h3>
+      {listingData.listingTitle}
+      {listingData.listingAddress}
+      {listingData.listingDescription}
+      ada accessible:
+      {' '}
+      {listingData.ada}
+      internet:
+      {' '}
+      {listingData.internet}
+      pets:
+      {' '}
+      {listingData.pets}
+      privateBath:
+      {' '}
+      {listingData.privateBath}
+      roommates:
+      {' '}
+      {listingData.roommates}
+      smoking:
+      {' '}
+      {listingData.smoking}
+    </div>
+  );
+};
 
 export default ListingInfo;
