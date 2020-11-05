75a0a46ab0073cd29756f7dfa14833d9ebb2c1f0
Jess Richmond <yesjessrichmond@gmail.com>
2020-11-04 22:08:04 -0600

    (add) availabilities for listings shown randomly in default search view

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -27,21 +27,35 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
 const Search: React.FC = () => {
   const classes = useStyles();
   const [locationQuery, setLocationQuery] = useState('');
-  const [listings, setListings] = useState([]);
+  const [listings, setListings] = useState([] as any);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [dateRange, setDateRange] = useState({
     start: moment().format('YYYY-MM-DD'),
     end: moment().add(7, 'days').format('YYYY-MM-DD'),
   });
-
   // view all listings in default search view
   // pass the setter to resultsList; will be updated via search there
   const getListings = () => {
     axios.get('/listing')
       .then((results) => {
-        const savedListings = results.data;
-        setListings(savedListings);
+        const savedListings = results.data.slice();
+        const listingTotal = savedListings.length;
+        const randomListings: number[] = [];
+        const resultsToDisplay: unknown[] = [];
+        while (randomListings.length < 4) {
+          const random = Math.floor(Math.random() * Math.floor(listingTotal));
+          if (!randomListings.includes(random)) randomListings.push(random);
+        }
+        for (let i = 0; i < randomListings.length; i += 1) {
+          const id = randomListings[i];
+          savedListings[id].availabilities = {
+            startDate: savedListings[id].availabilities[0].startDate,
+            endDate: savedListings[id].availabilities[0].endDate,
+          };
+          resultsToDisplay.push(savedListings[id]);
+        }
+        setListings(resultsToDisplay);
       })
       .catch((err) => err);
   };
--- src/components/search/SearchResultsListEntry.tsx
+++ src/components/search/SearchResultsListEntry.tsx
@@ -22,10 +22,33 @@ import { Link } from 'react-router-dom';
 
 const ResultsListEntry = (props: any) => {
   const {
-    user, title, location, avail,
+    user, title, location, avail, defaultView, availForDefault,
   } = props;
   const { listingCity, listingState } = location;
+  // this is for the listings that are randomly shown in the default view
+  const { startDate, endDate } = availForDefault;
+  // this is for the listings that match the query
   const { startAvail, endAvail } = avail;
+
+  let availMessage;
+  if (defaultView) {
+    availMessage = (
+      <p>
+        available as soon as
+        {startDate}
+      </p>
+    );
+  } else if (!defaultView) {
+    availMessage = (
+      <p>
+        available from
+        {startAvail}
+        {' '}
+        to
+        {endAvail}
+      </p>
+    );
+  }
 
   return (
     <ListItem
@@ -36,13 +59,7 @@ const ResultsListEntry = (props: any) => {
         primary={title}
         secondary={`${listingCity}, ${listingState}`}
       />
-      available from
-      {' '}
-      {startAvail}
-      {' '}
-      to
-      {' '}
-      {endAvail}
+      {availMessage}
     </ListItem>
   );
 };
