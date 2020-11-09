296bd6e7f690a887ab7b470ad4b33694d99c0a0d
Jess Richmond <yesjessrichmond@gmail.com>
2020-11-09 01:23:22 -0600

    (fix) no matching results(fix) no matching results(fix) no matching results(fix) no matching results(fix) no matching results(fix) no matching results(fix) no matching results(fix) no matching results(fix) no matching results

--- src/components/search/SearchResultsList.tsx
+++ src/components/search/SearchResultsList.tsx
@@ -32,6 +32,7 @@ const ResultsList = (props: any) => {
     locationQuery, listings, setListings, dateRange,
     defaultView, setLocationQuery, updated, setUpdated,
   } = props;
+  const [message, setMessage] = useState('');
 
   // use listing ids to render list of listings
   const getAvailListingInfo = (listingIds: any) => {
@@ -67,7 +68,8 @@ const ResultsList = (props: any) => {
         // if there are no available listings, inform the user
         if (!availListings.length) {
           // TODO: render this message
-          console.error(`sorry :/ there doesn't seem to be any available listings in ${locationQuery} from ${start} to ${end}`);
+          setListings([]);
+          setMessage(`sorry :/ there doesn't seem to be any available listings in ${locationQuery} from ${start} to ${end}`);
         } else {
         // if there are availabilities, collect the listing IDs of those availabilities
         // push that collection of IDs into an array called listingsToRender
@@ -96,6 +98,7 @@ const ResultsList = (props: any) => {
 
   return (
     <div className={classes.root}>
+      {message}
       <GridList className={classes.gridList} cols={2.5}>
         {listings.map((listing: {
           id: any; user_id: any; listingTitle: any;
