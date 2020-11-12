ed2f0560cfa40f5b2611d732d7f5c3b7f0577b48
Greg <gregthompson27@gmail.com>
2020-11-11 21:57:52 -0600

    (add) front end routes to get image url and send to db

--- server/index.js
+++ server/index.js
@@ -25,15 +25,18 @@ const { reviewRouter } = require('./db/routes/reviewRoutes');
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true,
+}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cors());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 const DIR = path.join(__dirname, '../build');
 const htmlFile = path.join(DIR, 'index.html');
 app.use(express.static(DIR));
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.engine('html', require('ejs').renderFile);
 
 app.set('view engine', 'html');
