79a6abdf95e1863120a4b2ba61d8fda532fe914c
Jess Richmond <yesjessrichmond@gmail.com>
2020-11-13 01:58:54 -0600

    (add) styling to bulletin board

--- src/components/bulletin/CreatePost.tsx
+++ src/components/bulletin/CreatePost.tsx
@@ -1,55 +1,26 @@
 import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
-export default function FormDialog() {
+const CreatePost: React.FC = (): JSX.Element => {
   const [open, setOpen] = useState(false);
   const [postBody, setPostBody] = useState('');
 
-  const handleClickOpen = () => {
-    setOpen(true);
+  const submit = () => {
+    console.log('submitted');
+    // TODO: why is userId in brackets?
   };
 
-  const handleClose = () => {
-    setOpen(false);
-    // axios.post('/bulletin');
-  };
-
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    console.log(postBody);
-  };
-
+  // TODO: styling & post request
   return (
-    <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        create post
-      </Button>
-      <form className="create" onSubmit={handleSubmit}>
-        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-          <DialogTitle id="form-dialog-title">say something! be nice!</DialogTitle>
-          <DialogContent>
-            <TextField
-              autoFocus
-              value={postBody}
-              label="Post"
-              type="text"
-              // onInput={(e) => setPostBody((<HTMLTextAreaElement>e.target).value)}
-              fullWidth
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button type="submit" color="primary">
-              submit
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </form>
+    <div className="holder">
+      <Grid item xs={12}>
+        <TextField id="outlined-search" type="search" variant="outlined" fullWidth />
+      </Grid>
     </div>
   );
-}
+};
+
+export default CreatePost;
