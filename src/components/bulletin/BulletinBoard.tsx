21cf8e33b93f43200a7872b1c37c5fbadcdf8526
Jess Richmond <yesjessrichmond@gmail.com>
2020-11-13 01:58:54 -0600

    (add) styling to bulletin board

--- src/components/bulletin/BulletinBoard.tsx
+++ src/components/bulletin/BulletinBoard.tsx
@@ -3,12 +3,31 @@ import axios from 'axios';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Post from './Post';
-import FormDialog from './CreatePost';
+import CreatePost from './CreatePost';
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   root: {
     flexGrow: 1,
     padding: '10px',
+  },
+  container: {
+    backgroundColor: 'red',
+    margin: 'auto',
+    display: 'flex',
+    justify: 'space-around',
+  },
+  sidebar: {
+    backgroundColor: 'green',
+  },
+  bulletins: {
+    backgroundColor: 'yellow',
+    align: 'center',
+    alignItems: 'center',
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
   },
 }));
 
@@ -16,6 +35,10 @@ const BulletinBoard: React.FC = (): JSX.Element => {
   const classes = useStyles();
   const [posts, setPosts] = useState([{
     body: '',
+    title: '',
+    user: { first_name: '', listing: { listingCity: '' } },
+    userId: 0,
+    userCity: '',
   }]);
 
   const getBulletins = () => {
@@ -32,13 +55,29 @@ const BulletinBoard: React.FC = (): JSX.Element => {
   return (
     <div className={classes.root}>
       <h4>community bulletins</h4>
-      <FormDialog />
-      <Grid container spacing={3}>
-        {posts.map((listing) => (
-          <Grid item xs={2}>
-            <Post body={listing.body} />
-          </Grid>
-        ))}
+      <Grid container className={classes.container} xs={11} spacing={3}>
+        <Grid container item className={classes.sidebar} xs={3}>
+          <CreatePost />
+        </Grid>
+        <Grid container item className={classes.bulletins} xs={9}>
+          {posts.map((post, idx) => {
+            const user = post.user.first_name;
+            const userCity = post.user.listing.listingCity;
+            const { title, body, userId } = post;
+            return (
+              <Grid item container className={classes.paper} xs={3}>
+                <Post
+                  title={title}
+                  body={body}
+                  author={user}
+                  authorId={userId}
+                  location={userCity}
+                  idx={idx}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
       </Grid>
     </div>
   );
--- src/components/bulletin/Post.tsx
+++ src/components/bulletin/Post.tsx
@@ -1,30 +1,137 @@
-import React, { useEffect } from 'react';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect } from 'react';
+import {
+  createStyles, Theme, makeStyles, MuiThemeProvider, createMuiTheme,
+} from '@material-ui/core/styles';
+
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 interface BulletinProps {
+  title: string,
   body: string,
+  author: string,
+  authorId: number,
+  location: string,
+  idx: number,
 }
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  root: {
-    width: '100%',
-    maxWidth: '36ch',
+const bulletinTheme = createMuiTheme({
+  typography: {
+    h6: {
+      fontWeight: 'bold',
+      lineHeight: 1.65,
+    },
+    subtitle1: {
+      lineHeight: 1.25,
+    },
+    overline: {
+      color: 'black',
+    },
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'left',
-    color: theme.palette.text.secondary,
+});
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  even: {
+    backgroundColor: 'pink',
+    height: '270px',
+    border: 'black 1px',
+    paddingLeft: '15px',
+    paddingRight: '15px',
+    paddingTop: '9px',
+  },
+  odd: {
+    backgroundColor: 'white',
+    height: '225px',
+    border: 'black 1px',
+    paddingLeft: '15px',
+    paddingRight: '15px',
+    paddingTop: '7px',
+  },
+  title: {
+    fontWeight: 'bold',
+    lineHeight: '1.25px',
+  },
+  body: {
+    lineHeight: '1.25px',
+  },
+  user: {
+
   },
 }));
 
-const Post: React.FC<BulletinProps> = ({ body }): JSX.Element => {
+const Post: React.FC<BulletinProps> = ({
+  idx, title, body, author, authorId, location,
+}): JSX.Element => {
   const classes = useStyles();
+  const [userId] = useState(localStorage.userId);
+  const [hostData, setHostData] = useState({
+    firstName: '',
+    lastName: '',
+    pronouns: '',
+    hostRating: '',
+    id: 0,
+    profilePhoto: '',
+    userBio: '',
+  });
+
+  const getAuthorInfo = () => {
+    axios.get(`/user/${authorId}`)
+      .then(({ data }) => {
+        const {
+          firstName,
+          lastName,
+          pronouns,
+          hostRating,
+          id,
+          profilePhoto,
+          userBio,
+        } = data;
+        setHostData({
+          firstName,
+          lastName,
+          pronouns,
+          hostRating,
+          id,
+          profilePhoto,
+          userBio,
+        });
+      })
+      .catch((err) => err);
+  };
+
+  useEffect(() => {
+    getAuthorInfo();
+  }, []);
 
   return (
-    <Paper className={classes.paper}>
-      {body}
-    </Paper>
+    <MuiThemeProvider theme={bulletinTheme}>
+      <Paper className={idx % 2 === 0 ? classes.even : classes.odd}>
+        <Grid item xs={12}><Typography variant="h6">{title}</Typography></Grid>
+        <Grid item xs={12}><Typography variant="subtitle1">{body}</Typography></Grid>
+        <Grid
+          item
+          xs={12}
+        >
+          <Typography
+            variant="overline"
+            component={Link}
+            to={
+            {
+              pathname: '/hostProfile',
+              state: { hostData, userId },
+            }
+          }
+          >
+            {author}
+          </Typography>
+          <Typography variant="overline">{location}</Typography>
+        </Grid>
+
+      </Paper>
+    </MuiThemeProvider>
   );
 };
 
