95edd1cc0b4ddf7d0a2c4c0d62e5cbce43cb8787
Greg <gregthompson27@gmail.com>
2020-11-13 17:12:08 -0600

    (refactor) personality routes and model to work with user model

--- server/db/Models/user.js
+++ server/db/Models/user.js
@@ -28,6 +28,7 @@ module.exports = (sequelize, DataTypes) => {
     User.hasMany(models.Message);
     User.hasMany(models.Bulletin);
     User.hasMany(models.Reviews);
+    User.hasOne(models.PersonalityScale);
   };
 
   return User;
