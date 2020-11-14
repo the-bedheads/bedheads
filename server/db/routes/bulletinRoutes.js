fbb48f546de3b1d950c0d24d7923bbb0e77465fc
Jess Richmond <yesjessrichmond@gmail.com>
2020-11-13 23:32:10 -0600

    (fix) styling + post functionality

--- server/db/routes/bulletinRoutes.js
+++ server/db/routes/bulletinRoutes.js
@@ -12,6 +12,7 @@ bulletinRouter.get('/', (req, res) => {
   Bulletin.findAll({
     include: {
       model: User,
+      as: 'author',
       include: {
         model: Listing,
       },
@@ -19,6 +20,17 @@ bulletinRouter.get('/', (req, res) => {
   })
     .then((posts) => res.send(posts))
     .catch((err) => res.send(err.message));
+});
+
+bulletinRouter.post('/', (req, res) => {
+  const { userId, title, body } = req.body;
+  Bulletin.create({
+    userId,
+    title,
+    body,
+  })
+    .then(() => res.send('success adding bulletin!'))
+    .catch(() => res.send('error adding bulletin'));
 });
 
 module.exports = {
